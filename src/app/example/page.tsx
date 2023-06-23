f5a9db0319e726416b2364c2171a34027d3ef246
josephgs-koo <josephgs.koo@gmail.com>
2023-06-24 00:36:03 +0900

    feat: main 페이지 skeleton작성

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -6,8 +6,8 @@ export default function Example() {
   return (
     <div className="flex flex-col justify-center w-3/4 gap-4 p-4 mx-auto">
       <div>
-        <span className='mb-2 font-bold'>Button Components</span>
-        <div className='flex justify-between w-full'>
+        <span className="mb-2 font-bold">Button Components</span>
+        <div className="flex justify-between w-full">
           <Btn className="w-48" shape="basic" onClick={() => console.log('click')}>
             basic
           </Btn>
---
+++ src/components/server/main/ResumeCardLoad.tsx
@@ -0,0 +1,19 @@
+export default function ResumeCardLoad() {
+  return (
+    <div className="flex flex-col justify-around w-2/5 gap-2 p-4 shadow-xl aspect-video rounded-2xl bg-neutral-200 animate-pulse min-w-[300px]">
+      <div className="flex flex-col w-full gap-2 h-1/4">
+        <div className="w-1/3 h-3 bg-white rounded-full" />
+        <div className="w-full h-3 bg-white rounded-full" />
+      </div>
+      <div className="flex flex-row items-center justify-between w-full gap-4 p-2 h-3/4">
+        <div className="bg-white rounded-full h-4/5 aspect-square" />
+        <div className="flex flex-col items-center justify-center flex-auto h-full gap-2">
+          <div className="w-full h-3 bg-white rounded-full" />
+          <div className="w-full h-3 bg-white rounded-full" />
+          <div className="w-full h-3 bg-white rounded-full" />
+          <div className="w-full h-3 bg-white rounded-full" />
+        </div>
+      </div>
+    </div>
+  )
+}
