08b00f6c8170e02228df5e8448a4d247c43afb3c
신창영 <115862267+SHINCHANGYOUNG@users.noreply.github.com>
2023-06-30 12:41:08 +0900

    feat: 이력서 작성 페이지에 자기소개 인풋창 추가

--- src/components/client/resumeld/CreateProfile.tsx
+++ src/components/client/resumeld/CreateProfile.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+'use client'
+
+/* eslint-disable react/no-array-index-key */
 import Titlename from '@/components/server/Titlename'
-import { SetStateAction, ChangeEvent, useState, FormEvent } from 'react'
+import { ChangeEvent, useState, FormEvent } from 'react'
 
 interface BlogInput {
   blog: string
@@ -12,6 +15,7 @@ export default function CreateProfile() {
   const [name, setName] = useState<string>('')
   const [bio, setBio] = useState<string>('')
   const [blogInputs, setBlogInputs] = useState<BlogInput[]>([{ blog: '', url: '' }])
+  const [introduce, setIntroduce] = useState<string>('')
 
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value)
@@ -35,6 +39,10 @@ export default function CreateProfile() {
     const updatedInputs = [...blogInputs]
     updatedInputs.splice(index, 1)
     setBlogInputs(updatedInputs)
+  }
+
+  const handleIntroduceChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setIntroduce(e.target.value)
   }
 
   const handleSubmit = (e: FormEvent) => {
@@ -113,18 +121,7 @@ export default function CreateProfile() {
       </div>
       <Titlename title="Introduce" />
       <div className="border border-gray-300 rounded-md p-4">
-        <span contentEditable>
-          {' '}
-          {/* Made the span contentEditable */}
-          8년 차 개발자로 스타트업에서 웹과 앱 서비스를 개발/배포/운영하였습니다. 주로 웹 개발을 담당했으며 필요에 따라
-          PO(Product Owner) 역할을 겸하였습니다. 작은 규모의 팀에 첫 번째 개발자로 입사하여 50억 투자를 유치하고 40명
-          규모의 팀으로 성장할 때까지 필요한 기술 역량을 책임졌던 경험이 있습니다. 비즈니스 성장에 기여하는 일을
-          최우선으로 합니다. 이를 위해 빠르게 개발하고 배포하여 딜리버리하는 역량과 데이터를 활용하여 유저를 이해하는
-          역량을 쌓고자 노력해왔습니다. 또한, 큰 임팩트는 혼자가 아닌 함께 만들 수 있다고 생각하기에 다양한 직무의
-          구성원들과 적극적으로 커뮤니케이션하며 협업해왔습니다. 반복되는 일을 자동화하고 비효율적인 프로세스를 개선하는
-          일을 좋아합니다. 운영팀의 단순/루틴 업무 시간을 줄이기 위해 지속적으로 백오피스를 개발하여 운영팀 업무 시간을
-          주당 70시간 이상 줄였던 경험이 있습니다.
-        </span>
+        <input type="text" value={introduce} onChange={handleIntroduceChange} className="outline-none" />
       </div>
       <button type="submit" className="bg-blue-500 text-white mt-4 py-2 px-4 rounded">
         저장
