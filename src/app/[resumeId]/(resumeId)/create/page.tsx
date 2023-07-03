e4f3780345449cb366abde7664edd56966f58a7a
신창영 <115862267+SHINCHANGYOUNG@users.noreply.github.com>
2023-07-03 14:59:34 +0900

    feat: 이력서 크리에트 부분 리엑트 훅 폼 으로 고치는 중

--- src/app/[resumeId]/(resumeId)/create/page.tsx
+++ src/app/[resumeId]/(resumeId)/create/page.tsx
@@ -4,18 +4,13 @@
 /* eslint-disable import/order */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
-import CreateProfile, { Field } from '@/components/client/resumeld/CreateProfile'
-import { createProfile } from '@/pages/api/resumeld/[createProfile]'
+import CreateProfile from '@/components/client/resumeld/CreateProfile'
 
 export default function CreateProfilePage() {
-  const handleSubmit = (name: string, description: string, fields: Field[]) => {
-    createProfile(name, description, fields)
-  }
-
   return (
     <div>
       <h1>Create Profile</h1>
-      <CreateProfile onSubmit={handleSubmit} />
+      <CreateProfile />
     </div>
   )
 }
