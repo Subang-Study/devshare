f171c6999b2e35f16b0945d01279370bde81735e
신창영 <115862267+SHINCHANGYOUNG@users.noreply.github.com>
2023-07-07 13:12:35 +0900

    feat: 유저프로필 부분 테이블 수정

--- src/components/client/resumeld/UserImageUploadButton.jsx
+++ src/components/client/resumeld/UserImageUploadButton.jsx
@@ -1,9 +1,18 @@
 'use client'
 
-import React, { useState } from 'react'
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/button-has-type */
+/* eslint-disable jsx-a11y/img-redundant-alt */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/jsx-no-useless-fragment */
+import React, { useState, useRef } from 'react'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function UserImageUploadButton({ onImageUpload }) {
   const [selectedFile, setSelectedFile] = useState(null)
+  const fileInputRef = useRef(null)
 
   const handleImageUpload = async (e) => {
     const file = e.target.files[0]
@@ -11,6 +20,14 @@ export default function UserImageUploadButton({ onImageUpload }) {
     // 파일 업로드 로직 작성
     // 업로드 성공 시 이미지 URL을 폼 데이터에 설정
     // setValue('userInfo.userImage', imageUrl);
+  }
+
+  const handleClickImage = () => {
+    fileInputRef.current.click()
+  }
+
+  const handleReloadImage = () => {
+    setSelectedFile(null)
   }
 
   return (
@@ -21,10 +38,20 @@ export default function UserImageUploadButton({ onImageUpload }) {
           className="bg-blue-500 w-full aspect-square flex items-center justify-center cursor-pointer"
         >
           파일 업로드
-          <input id="imageUpload" type="file" accept="image/*" onChange={handleImageUpload} className="hidden" />
+          <input
+            id="imageUpload"
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+            className="hidden"
+            ref={fileInputRef}
+          />
         </label>
       ) : (
-        <img src={URL.createObjectURL(selectedFile)} alt="Uploaded Image" />
+        <>
+          <img src={URL.createObjectURL(selectedFile)} alt="Uploaded Image" onClick={handleClickImage} />
+          <button onClick={handleReloadImage}>재업로드</button>
+        </>
       )}
     </>
   )
--- src/components/client/resumeld/UserInfoForm.tsx
+++ src/components/client/resumeld/UserInfoForm.tsx
@@ -1,88 +1,6 @@
 'use client'
 
-// import { useFormContext, useFieldArray } from 'react-hook-form'
-// import { IResumeData } from '@/types/resumeDataType'
-// import { k2d } from '@/app/font'
-
-// export default function UserInfoForm() {
-//   const { register, control } = useFormContext<IResumeData>()
-//   const { fields, append, remove } = useFieldArray({
-//     control,
-//     name: 'userInfo.personal.channel',
-//   })
-
-//   return (
-//     <>
-//       <input
-//         placeholder="이름"
-//         type="text"
-//         {...register('userInfo.name', { required: true })}
-//         className="text-[2rem] font-bold mt-4    outline-none "
-//       />
-//       <input
-//         placeholder="한줄 자기소개"
-//         type="text"
-//         {...register('userInfo.sentense', { required: true })}
-//         className="w-full text-gray-600    outline-none"
-//       />
-//       <div className="flex flex-row justify-between gap-4 items-center mt-4 md:flex-wrap w-full">
-//         <div className="w-1/4 min-w-[200px]">
-//           <div className="bg-blue-500 w-full aspect-square flex items-center justify-center">USERIMAGE</div>
-//         </div>
-//         <div className="min-w-[400px] h-full text-gray-700 flex flex-col flex-auto">
-//           <div>
-//             Email{' '}
-//             <input
-//               placeholder="이름"
-//               type="text"
-//               {...register('userInfo.personal.email', { required: true })}
-//               className="  outline-none"
-//             />
-//           </div>
-//           <div>
-//             Phone{' '}
-//             <input
-//               placeholder="010-0000-0000"
-//               type="text"
-//               {...register('userInfo.personal.phone', { required: true })}
-//               className="  outline-none"
-//             />
-//             <button type="button" onClick={() => append({ title: '', url: '' })}>
-//               +
-//             </button>
-//           </div>
-//           {fields.map((field, idx) => {
-//             return (
-//               <div key={field.id}>
-//                 <input
-//                   placeholder="ex)깃허브"
-//                   type="text"
-//                   {...register(`userInfo.personal.channel.${idx}.title`)}
-//                   className=" border-gray-400 focus:border-blue-500 outline-none"
-//                 />
-//                 <input placeholder="url" type="text" {...register(`userInfo.personal.channel.${idx}.url`)} />
-//                 <button onClick={() => remove(idx)} type="button">
-//                   -
-//                 </button>
-//               </div>
-//             )
-//           })}
-//         </div>
-//       </div>
-//       <h3 className={`${k2d.className} text-blue-600 text-3xl`}>Introduce</h3>
-//       <hr className="w-full h-0.5 bg-black border-none rounded-full" />
-//       <div className="border border-gray-300 rounded-md mt-4 p-4">
-//         <textarea
-//           rows={5}
-//           placeholder=""
-//           {...register('userInfo.introduction', { required: true })}
-//           className="outline-none w-full gap-1 "
-//         />
-//       </div>
-//     </>
-//   )
-// }
-
+/* eslint-disable import/extensions */
 import { useFormContext, useFieldArray } from 'react-hook-form'
 import { IResumeData } from '@/types/resumeDataType'
 import { k2d } from '@/app/font'
@@ -126,7 +44,7 @@ export default function UserInfoForm() {
                 <td>Email</td>
                 <td>
                   <input
-                    placeholder="이름"
+                    placeholder="email"
                     type="text"
                     {...register('userInfo.personal.email', { required: true })}
                     className="  outline-none"
@@ -144,24 +62,30 @@ export default function UserInfoForm() {
                   />
                 </td>
               </tr>
+
+              {fields.map((field, idx) => {
+                return (
+                  <tr key={field.id}>
+                    <td className="w-40">
+                      <input
+                        placeholder="ex)깃허브"
+                        type="text"
+                        {...register(`userInfo.personal.channel.${idx}.title`)}
+                        className="w-full outline-none"
+                      />
+                    </td>
+                    <td>
+                      {' '}
+                      <input placeholder="url" type="text" {...register(`userInfo.personal.channel.${idx}.url`)} />
+                      <button onClick={() => remove(idx)} type="button">
+                        -
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
             </tbody>
           </table>
-          {fields.map((field, idx) => {
-            return (
-              <div key={field.id}>
-                <input
-                  placeholder="ex)깃허브"
-                  type="text"
-                  {...register(`userInfo.personal.channel.${idx}.title`)}
-                  className=" border-gray-400 focus:border-blue-500 outline-none"
-                />
-                <input placeholder="url" type="text" {...register(`userInfo.personal.channel.${idx}.url`)} />
-                <button onClick={() => remove(idx)} type="button">
-                  -
-                </button>
-              </div>
-            )
-          })}
           <div>
             <button type="button" onClick={() => append({ title: '', url: '' })}>
               +
@@ -169,7 +93,7 @@ export default function UserInfoForm() {
           </div>
         </div>
       </div>
-      <h3 className={`${k2d.className} text-blue-600 text-3xl`}>Introduce</h3>
+      <h3 className={`${k2d.className} text-3xl`}>Introduce</h3>
       <hr className="w-full h-0.5 bg-black border-none rounded-full" />
       <div className="border border-gray-300 rounded-md mt-4 p-4">
         <textarea
