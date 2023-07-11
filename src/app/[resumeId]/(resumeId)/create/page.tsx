cca61f2a0be3323050631a2665317a6cfabc7dbf
Koo Gyo Seok <48327655+josephgs-koo@users.noreply.github.com>
2023-07-11 11:49:26 +0900

    Revert "Feat/imageupload"

--- src/app/[resumeId]/(resumeId)/create/page.tsx
+++ src/app/[resumeId]/(resumeId)/create/page.tsx
@@ -1,16 +1,3 @@
-'use client'
-
-/* eslint-disable import/extensions */
-/* eslint-disable import/order */
-/* eslint-disable import/no-extraneous-dependencies */
-import React from 'react'
-import CreateProfile from '@/components/client/resumeld/CreateProfile'
-
-export default function CreateProfilePage() {
-  return (
-    <div>
-      <h1>Create Profile</h1>
-      <CreateProfile />
-    </div>
-  )
+export default function CreateResume() {
+  return <div>resume create</div>
 }
--- src/app/[resumeId]/page.tsx
+++ src/app/[resumeId]/page.tsx
@@ -1,18 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import Profile from '@/components/server/Profile'
-import Introduce from '@/components/server/Introduce'
-import Skillset from '@/components/server/Skillset'
-import Experience from '@/components/server/Experience'
-import Study from '@/components/server/Study'
-
 export default function Resume() {
-  return (
-    <>
-      <Profile />
-      <Introduce />
-      <Skillset />
-      <Experience />
-      <Study />
-    </>
-  )
+  return <div>resume</div>
 }
