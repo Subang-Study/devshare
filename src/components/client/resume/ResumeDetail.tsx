a90ba7ec08a1521247319e46ac6c4809913e7d8e
신창영 <115862267+SHINCHANGYOUNG@users.noreply.github.com>
2023-09-18 13:55:21 +0900

    resume file 정리및 디자인패턴 적용

--- src/components/client/resume/ResumeDetail.tsx
+++ src/components/client/resume/ResumeDetail.tsx
@@ -1,9 +1,9 @@
 'use client'
 
-import Categories from '@/components/server/resume/Categories'
-import Introduce from '@/components/server/resume/Introduce'
+import Category from '@/components/server/resume/Category'
 import Profile from '@/components/server/resume/Profile'
-import Skillset from '@/components/server/resume/Skillset'
+import Skill from '@/components/server/resume/Skill'
+import CategoryDetails from '@/components/server/resume/CategoryDetails'
 import { Session } from 'next-auth'
 import { useQuery } from '@tanstack/react-query'
 import { getPost } from '@/lib/api/post'
@@ -25,9 +25,32 @@ export default function ResumeDetails({ session, id }: IResumeDetailProps) {
       <div className="relative flex flex-col gap-3">
         {session?.user.id === data.author && <ResumeOwnerBtns resumeId={id} />}
         <Profile profileData={data.userInfo} />
-        <Introduce profileData={data.userInfo} />
-        <Skillset techData={data.techStack} />
-        <Categories categoryList={data.categorys} />
+        {/* 자기소개 */}
+        <Category title="Introduce">
+          <Category.Direction direction="none">
+            <p className="w-full gap-1">{data.userInfo.introduction}</p>
+          </Category.Direction>
+        </Category>
+        {/* 스킬 셋 */}
+        <Category title="Skill Set">
+          <Category.Direction direction="horizon">
+            {data.techStack?.map((el) => (
+              <Skill key={el.title} title={el.title} description={el.description} />
+            ))}
+          </Category.Direction>
+        </Category>
+        {/* 각종 카테고리 */}
+        {data.categorys?.map((el) => {
+          return (
+            <Category title={el.title} key={el.title}>
+              <Category.Direction direction="vertical">
+                {el.detail.map((detail) => {
+                  return <CategoryDetails detailData={detail} key={detail.title} />
+                })}
+              </Category.Direction>
+            </Category>
+          )
+        })}
       </div>
     )
   }
